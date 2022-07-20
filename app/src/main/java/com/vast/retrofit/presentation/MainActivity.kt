0a8014e77caa462c0e03ef865181c41dce192431
Valeriy Smirnov <smirnov@atlasbiomed.com>
2022-07-20 14:05:37 +0300

    add project files

---
+++ app/src/main/java/com/vast/retrofit/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vast.retrofit.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vast.retrofit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
